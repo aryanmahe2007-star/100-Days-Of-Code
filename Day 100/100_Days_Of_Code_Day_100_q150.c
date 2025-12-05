192d5ce6f6e24ef2cc75ee8ad3694d3407822a4f
aryanmahe2007-star <aryanmahe2007@gmail.com>
2025-12-05 15:55:04 +0530

    100 Days Of Code

---
+++ Day 100/100_Days_Of_Code_Day_100_q150.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    float marks;
+};
+
+int main() {
+    struct Student s1;
+    struct Student *ptr;
+
+    ptr = &s1;
+
+    printf("Enter name: ");
+    scanf("%s", ptr->name);
+
+    printf("Enter roll number: ");
+    scanf("%d", &ptr->roll);
+
+    printf("Enter marks: ");
+    scanf("%f", &ptr->marks);
+
+    printf("\n--- Student Details ---\n");
+    printf("Name  : %s\n", ptr->name);
+    printf("Roll  : %d\n", ptr->roll);
+    printf("Marks : %.2f\n", ptr->marks);
+
+    return 0;
+}
